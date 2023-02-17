2e5926f14799f75347e54e5af2fcb89de31c17c9
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-17 19:41:00 +0300

    contract.js improvements 🛠
    • Used ES6 import syntax to import the individual functions.
    • Simplified the if statement for input validation and error handling.
    • Created a separate object functionMap to map input functions to their corresponding function implementations.
    • Added error handling for the case where the input function is not in the function map.
    • Renamed the change-price function to changePrice to follow standard camel case naming conventions.

--- src/contract.js
+++ src/contract.js
@@ -1,19 +1,32 @@
+import transfer from "./functions/transfer.js";
+import list from "./functions/list.js";
+import balance from "./functions/balance.js";
+import changePrice from "./functions/change-price.js";
+import reserveBuyingZone from "./functions/reserve-buying-zone.js";
+import unlist from "./functions/unlist.js";
+import finalizeBuy from "./functions/finalize-buy.js";
+
 export async function handle(state, action) {
-    let { input, caller } = action
+  const { input, caller } = action;
 
-    if (!input || typeof input !== "object" || !input.function || typeof input.function !== "string") {
-        throw new ContractError("No function")
-    }
+  if (!input || typeof input !== "object" || typeof input.function !== "string") {
+    throw new ContractError("Invalid input");
+  }
 
-    return await (({
-        "transfer": require("./functions/transfer.js"),
-        "list": require("./functions/list.js"),
-        "balance": require("./functions/balance.js"),
-        "change-price": require("./functions/change-price.js"),
-        "reserve-buying-zone": require("./functions/reserve-buying-zone.js"),
-        "unlist": require("./functions/unlist.js"),
-        "finalize-buy": require("./functions/finalize-buy.js")
-    })[input.function] || (function invalidFunc() { throw new ContractError("Invalid function") }))(state, action)
+  const functionMap = {
+    "transfer": transfer,
+    "list": list,
+    "balance": balance,
+    "change-price": changePrice,
+    "reserve-buying-zone": reserveBuyingZone,
+    "unlist": unlist,
+    "finalize-buy": finalizeBuy,
+  };
 
+  const selectedFunction = functionMap[input.function];
+  if (!selectedFunction) {
+    throw new ContractError("Invalid function");
+  }
 
+  return await selectedFunction(state, action);
 }
