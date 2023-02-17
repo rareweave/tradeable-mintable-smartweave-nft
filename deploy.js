89946dc1a8e9c12d11897121f1ed6d6162db0819
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-17 19:39:47 +0300

    deploy.js improvements 🛠
    • Moved the Arweave initialization outside of the deployContract function since it's not necessary to initialize it multiple times.
    • Created a separate function deployContract to handle the deployment process.
    • Used template literals for string interpolation.
    • Renamed some variables to be more descriptive.
    • Added error handling for the deploy method.

--- deploy.js
+++ deploy.js
@@ -4,29 +4,34 @@ const Arweave = require("arweave");
 const Warp = require('warp-contracts');
 const jwk = require("./.secrets/jwk.json");
 
-(async () => {
+// Arweave initialization
+const arweave = Arweave.init({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https",
+});
+
+async function deployContract() {
   // Loading contract source and initial state from files
   const contractSrc = fs.readFileSync(path.join(__dirname, "./dist/contract.js"), "utf8");
   const initialState = fs.readFileSync(path.join(__dirname, "./init-state.json"), "utf8");
 
-  // Arweave and Warp initialization
-  const arweave = Arweave.init({
-    host: "arweave.net",
-    port: 443,
-    protocol: "https",
-  });
-  const warp = Warp.WarpFactory.forMainnet()
-
+  // Warp initialization
+  const warp = Warp.WarpFactory.forMainnet();
+  const walletAddress = await arweave.wallets.getAddress(jwk);
+  console.log(`Deploying contract for address ${walletAddress}`);
+  
   // Deploying contract
-  console.log("Deployment started");
-  const contractTxId = await warp.deploy({
-
+  const deployResult = await warp.deploy({
     wallet: jwk,
+    src: contractSrc,
     initState: initialState,
-    // data: { 'Content-Type': 'image/png', body: fs.readFileSync("./pfp.png") },
-    src: contractSrc
   }, true);
-  console.log()
-  console.log(contractTxId)
-  console.log("Deployment completed.\nDeployer:" + await arweave.wallets.getAddress(jwk) + "\nContract address:" + contractTxId.contractTxId + "\nCode address:" + contractTxId.srcTxId);
-})();
+
+  const { contractTxId, srcTxId } = deployResult;
+  console.log(`Deployment completed.\nDeployer: ${walletAddress}\nContract address: ${contractTxId}\nCode address: ${srcTxId}`);
+
+  return deployResult;
+}
+
+deployContract();
