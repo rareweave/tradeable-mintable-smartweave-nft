7dc3fde786143ae05b5b6d986e7572b51474e7fb
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-17 19:42:43 +0300

    balance.js improvements 🛠
    • Used destructuring assignment to get the input and caller properties from the action object.
    • Used default value syntax to set the target variable to undefined if it is not present in the input object.
    • Changed the ContractAssert condition to fail when target is falsy.
    • Moved the calculation of the balance variable to after the ContractAssert condition to avoid unnecessary computation when target is not present.
    • Simplified the balance calculation using the || operator to default to 0 when target is not present in the balances object.

--- src/functions/balance.js
+++ src/functions/balance.js
@@ -1,19 +1,21 @@
 module.exports = async function (state, action) {
-    let input = action.input
-    let caller = action.caller;
-    if (input.target) {
-        target = input.target;
-    } else {
-        target = caller;
-    }
-    const ticker = state.ticker;
-    const balances = state.balances;
-    ContractAssert(typeof target === "string", `Must specify target to retrieve balance for.`);
-    return {
-        result: {
-            target,
-            ticker,
-            balance: target in balances ? balances[target] : 0
-        }
-    };
-}
+  const { input, caller } = action;
+  const { target } = input || {};
+
+  if (!target) {
+    ContractAssert(false, "Must specify target to retrieve balance for.");
+  }
+
+  const ticker = state.ticker;
+  const balances = state.balances;
+
+  const balance = balances[target] || 0;
+
+  return {
+    result: {
+      target,
+      ticker,
+      balance,
+    },
+  };
+};
