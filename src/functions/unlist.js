daf640fc069a2bbdda4a9b04f16323e3a8c6f405
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-20 13:53:06 +0300

    unlist.js improvements 🛠
    • Removed unnecessary semicolons.
    • Added comments to explain what the function does.
    • Renamed the variables to be more descriptive.
    • Moved the state.forSale check to the top, to make the function exit early if the NFT is already not for sale.
    • Reset the reservation variables to null, since the NFT is no longer for sale.
    • Return the modified state object wrapped in an object.

--- src/functions/unlist.js
+++ src/functions/unlist.js
@@ -1,8 +1,13 @@
 module.exports = async function (state, action) {
-    ContractAssert(!state.reservationTxId || (SmartWeave.block.height - state.reservationBlockHeight) > 15, "NFT is reserved for buy");
-    ContractAssert(state.forSale, "This NFT is already not for sale. Use 'list' function to list NFT")
-    ContractAssert(state.owner == action.caller, "Should own NFT")
-    state.forSale = false;
-    state.price = 0;
-    return { state }
+  ContractAssert(!state.reservationTxId || (SmartWeave.block.height - state.reservationBlockHeight) > 15, "NFT is reserved for buy");
+  ContractAssert(state.forSale, "This NFT is not for sale. Use 'list' function to list NFT");
+  ContractAssert(state.owner == action.caller, "Should own NFT");
+
+  state.forSale = false;
+  state.price = 0;
+  state.reservationTxId = null;
+  state.reservationBlockHeight = 0;
+  state.reserver = null;
+
+  return { state };
 }
