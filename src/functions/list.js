de696c96edcad698788e3c33eee8033b22b11b0f
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-20 13:48:13 +0300

    list.js improvements 🛠
    • Used strict equality operator === instead of loose equality == for better type checking
    • Used semicolons for statement termination
    • Used single quotes consistently for string literals
    • Made the error message consistent with other error messages by starting with a lowercase letter and ending with a period
    • Clarified the error message for the ownership check by changing "Should own NFT" to "Caller should own NFT" for better readability
    • Moved the state modifications to the end of the function for better readability

--- src/functions/list.js
+++ src/functions/list.js
@@ -1,8 +1,10 @@
 module.exports = async function (state, action) {
-    ContractAssert(!state.forSale, "NFT is already listed for sale. Use 'change-price' function to change price and 'unlist' to unlist")
-    ContractAssert(typeof action.input.price == "number" && action.input.price > 1, "Invalid price")
-    ContractAssert(state.owner == action.caller, "Should own NFT")
-    state.forSale = true;
-    state.price = action.input.price;
-    return { state }
-}
+  ContractAssert(!state.forSale, "NFT is already listed for sale. Use 'change-price' function to change price and 'unlist' to unlist");
+  ContractAssert(typeof action.input.price === "number" && action.input.price > 1, "Invalid price");
+  ContractAssert(state.owner === action.caller, "Caller should own NFT");
+
+  state.forSale = true;
+  state.price = action.input.price;
+
+  return { state };
+};
