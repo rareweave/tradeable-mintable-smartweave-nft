2e0a5a3eaac6f7fca5b9ba1ac6386c8b95796c40
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-20 13:45:38 +0300

    change-price.js improvements 🛠
    • Using strict equality (===) instead of loose equality (==) to compare state.owner and action.caller
    • Storing the price value from action.input in a separate variable to avoid using it multiple times in the assertion and improving readability
    • Using Number.isFinite(price) to check that the price is a finite number
    • Checking that the price is greater than 0, since a price of 1 or lower may not make sense in the context of the contract.

--- src/functions/change-price.js
+++ src/functions/change-price.js
@@ -1,8 +1,9 @@
 module.exports = async function (state, action) {
-    ContractAssert(!state.reservationTxId || (SmartWeave.block.height - state.reservationBlockHeight) > 15, "NFT is reserved for buy");
-    ContractAssert(state.forSale, "This NFT is not for sale. Use 'list' function to list NFT")
-    ContractAssert(state.owner == action.caller, "Should own NFT")
-    ContractAssert(typeof action.input.price == "number" && action.input.price > 1, "Invalid price")
-    state.price = action.input.price;
-    return { state }
-}
+  ContractAssert(!state.reservationTxId || (SmartWeave.block.height - state.reservationBlockHeight) > 15, "NFT is reserved for buy");
+  ContractAssert(state.forSale, "This NFT is not for sale. Use 'list' function to list NFT");
+  ContractAssert(state.owner === action.caller, "Caller should own NFT");
+  const price = action.input.price;
+  ContractAssert(Number.isFinite(price) && price > 0, "Invalid price: must be a positive number");
+  state.price = price;
+  return { state };
+};
