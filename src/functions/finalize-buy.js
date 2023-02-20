75cfeb3bfc9cfa9d377d4dfaeb95ed296f1cb3fc
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-02-20 13:42:31 +0300

    finalize-buy.js improvements 🛠
    • Extracted owner, caller, and input from the action to make the code more readable.
    • Improved the error message for the second assertion by clarifying that only the reserver can finalize the buy.
    • Reordered the assertions to make them easier to read.
    • Added semicolons to the end of the statements for consistency.

--- src/functions/finalize-buy.js
+++ src/functions/finalize-buy.js
@@ -1,17 +1,41 @@
-module.exports = async function (state, action) {
-    ContractAssert(state.forSale, 'NFT is not listed for sale')
-    ContractAssert(state.reserver == SmartWeave.transaction.owner)
-    ContractAssert(state.reservationTxId && (SmartWeave.block.height - state.reservationBlockHeight) < 15, "NFT is not reserved for buy");
-    ContractAssert(state.reservationTxId == action.input.reservationTxId, "Provided reservation txid is invalid")
-    ContractAssert(action.input.price == state.price, "Wanted price doesn't match listing price")
-    ContractAssert(SmartWeave.transaction.target == action.caller && SmartWeave.transaction.quantity == state.price, "Invalid transfer")
-    state.reservationBlockHeight = 0;
-    state.reservationTxId = null;
-    state.reserver = null
-    state.owner = action.caller
-    state.forSale = false;
-    state.price = 0;
-    state.balances = { [action.caller]: 1 }
-    return { state }
+module.exports = async function(state, action) {
+  const owner = SmartWeave.transaction.owner;
+  const caller = action.caller;
+  const input = action.input;
 
-}
+  ContractAssert(
+    state.forSale,
+    "NFT is not listed for sale"
+  );
+  ContractAssert(
+    state.reserver == owner,
+    "Only the reserver can finalize the buy"
+  );
+  ContractAssert(
+    state.reservationTxId &&
+      SmartWeave.block.height - state.reservationBlockHeight < 15,
+    "NFT is not reserved for buy"
+  );
+  ContractAssert(
+    state.reservationTxId == input.reservationTxId,
+    "Provided reservation txid is invalid"
+  );
+  ContractAssert(
+    input.price == state.price,
+    "Wanted price doesn't match listing price"
+  );
+  ContractAssert(
+    SmartWeave.transaction.target == caller && SmartWeave.transaction.quantity == state.price,
+    "Invalid transfer"
+  );
+
+  state.reservationBlockHeight = 0;
+  state.reservationTxId = null;
+  state.reserver = null;
+  state.owner = caller;
+  state.forSale = false;
+  state.price = 0;
+  state.balances = { [caller]: 1 };
+
+  return { state };
+};
