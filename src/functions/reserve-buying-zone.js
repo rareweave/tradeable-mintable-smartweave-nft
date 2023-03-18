4298571583e58cb2895f47ee44dfc690c17eca86
Angrymouse <rykov.nikas@gmail.com>
2023-03-18 19:48:43 +0200

    add rounding to royalty

--- src/functions/reserve-buying-zone.js
+++ src/functions/reserve-buying-zone.js
@@ -1,7 +1,7 @@
 module.exports = async function (state, action) {
   const { target, quantity, owner, id } = SmartWeave.transaction;
 
-  ContractAssert(target === state.minter && quantity == action.input.price * state.royalty, "Invalid burn");
+  ContractAssert(target === state.minter && quantity == Math.floor(action.input.price * state.royalty), "Invalid burn");
   ContractAssert(state.forSale, 'NFT is not listed for sale');
   ContractAssert(!state.reservationTxId || (SmartWeave.block.height - state.reservationBlockHeight) > 15, "NFT is reserved for buy");
   ContractAssert(action.input.price === state.price, "Wanted price doesn't match listing price");
