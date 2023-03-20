358500ab68c4080fca428befc7842f7d82debf88
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-03-20 16:33:02 +0300

    Update finalize-buy.js
    The code style has been changed to be more appropriate

--- src/functions/finalize-buy.js
+++ src/functions/finalize-buy.js
@@ -1,41 +1,18 @@
 module.exports = async function (state, action) {
-
-  const caller = action.caller;
-  const input = action.input;
-
-  ContractAssert(
-    state.forSale,
-    "NFT is not listed for sale"
-  );
-  ContractAssert(
-    state.reserver == caller,
-    "Only the reserver can finalize the buy"
-  );
-  ContractAssert(
-    state.reservationTxId &&
-    SmartWeave.block.height - state.reservationBlockHeight < 15,
-    "NFT is not reserved for buy"
-  );
-  ContractAssert(
-    state.reservationTxId == input.reservationTxId,
-    "Provided reservation txid is invalid"
-  );
-  ContractAssert(
-    input.price == state.price,
-    "Wanted price doesn't match listing price"
-  );
-  ContractAssert(
-    SmartWeave.transaction.target == state.owner && SmartWeave.transaction.quantity == state.price,
-    "Invalid transfer"
-  );
+  ContractAssert(state.forSale, "NFT is not listed for sale");
+  ContractAssert(state.reserver == action.caller, "Only the reserver can finalize the buy");
+  ContractAssert(state.reservationTxId && SmartWeave.block.height - state.reservationBlockHeight < 15, "NFT is not reserved for buy");
+  ContractAssert(state.reservationTxId == action.input.reservationTxId, "Provided reservation txid is invalid");
+  ContractAssert(action.input.price == state.price, "Wanted price doesn't match listing price");
+  ContractAssert(SmartWeave.transaction.target == state.owner && SmartWeave.transaction.quantity == state.price, "Invalid transfer");
 
   state.reservationBlockHeight = 0;
   state.reservationTxId = null;
   state.reserver = null;
-  state.owner = caller;
+  state.owner = action.caller;
   state.forSale = false;
   state.price = 0;
-  state.balances = { [caller]: 1 };
+  state.balances = { [action.caller]: 1 };
 
   return { state };
 };
