830b50558fbd56deee7f83ba6d453b48db6e1b23
Angrymouse <rykov.nikas@gmail.com>
2023-07-25 05:17:13 +0300

    fix royalty

--- src/functions/reserve-buying-zone.js
+++ src/functions/reserve-buying-zone.js
@@ -5,13 +5,13 @@ module.exports = async function (state, action) {
   ContractAssert(state.forSale, 'NFT is not listed for sale');
   ContractAssert(!state.reservationTxId || (SmartWeave.transaction.timestamp - state.reservationTimestamp) > 600000, "NFT is reserved for buy");
   ContractAssert(BigInt(action.input.price) >= BigInt(state.price), "Wanted price doesn't match listing price");
-  ContractAssert(typeof action.input.transferTxID=="string","No royalty tx")
+  ContractAssert(typeof action.input.transferTxID == "string", "No royalty tx")
   ContractAssert(SmartWeave.extensions[state.listingChain] && typeof SmartWeave.extensions[state.listingChain].readTxById == "function", "No " + state.listingChain + " plugin installed.")
   let fetchedRoyaltyTx = await SmartWeave.extensions[state.listingChain].readTxById(action.input.transferTxID)
 
   ContractAssert(fetchedRoyaltyTx.to == state.royaltyAddresses[state.listingChain], "Invalid transfer (address)")
-  ContractAssert(BigInt(fetchedRoyaltyTx.amount) >= (BigInt(state.price) / BigInt(parseInt(100 / state.royalty))), "Invalid royalty transfer amount")
-  ContractAssert(fetchedRoyaltyTx.coin==state.listingCoin,"Incorrect transfer coin")
+  ContractAssert(BigInt(fetchedRoyaltyTx.amount) >= (BigInt(state.price) / BigInt(parseInt(1 / state.royalty))), "Invalid royalty transfer amount")
+  ContractAssert(fetchedRoyaltyTx.coin == state.listingCoin, "Incorrect transfer coin")
   state.reservationTimestamp = SmartWeave.transaction.timestamp;
   state.reservationTxId = id;
   state.reserver = owner;
